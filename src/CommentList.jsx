4d221a6d2c4fcc28dffd9856b33fb10bb79ac906
chaeyoung <olivia0827@naver.com>
2025-04-04 02:17:59 +0900

    first-component

--- src/CommentList.jsx
+++ src/CommentList.jsx
@@ -7,7 +7,7 @@ function CommentList(props) {
       <Comment name={"멋쟁이"} comment={"내가 리액트 유저?"} />
       <Comment name={"아기사자"} comment={"즐거운 프론트 세션!!!!!!"} />
     
-      <Comment name={"천재사자"} comment={"아 코딩하고 싶다"}/>
+      <Comment name={"천재사자자자"} comment={"아 코딩하고 싶다"}/>
     </div>
 
   );
